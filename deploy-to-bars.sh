0f9ad3a254ea2178f9205d6b00d8f37a0b671dfe
jonny <387119@gmail.com>
2020-10-28 18:01:52 +0200

    changes

---
+++ deploy-to-bars.sh
@@ -0,0 +1,1 @@
+ansible-playbook -v deploy.yml -i ansible/hosts --limit cc-bars-srv1
--- deploy.sh
+++
@@ -1,2 +0,0 @@
-ssh home-hw1 'cd /root/binance/bot1/test && docker-compose down && git pull && docker-compose up -d'
-
